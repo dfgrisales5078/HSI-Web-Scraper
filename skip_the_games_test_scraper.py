3033822732e9ff5ff035ad13730f32caee058084
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2023-01-13 14:52:28 -0500

    moved run scaper to initialize

--- skip_the_games_test_scraper.py
+++ skip_the_games_test_scraper.py
@@ -2,13 +2,19 @@ import time
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import logging
+
+
+LOG_FILE = 'listings_results.log'
+logging.basicConfig(level=logging.INFO,
+                    datefmt="%m/%d/%Y %H:%M:%S", filename=LOG_FILE)
 
 
 class TestScraper:
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.location = 'fort-myers'
-        self.keywords = ['outcall', 'independent']
+        self.keywords = ['cash', 'Exotic']
         self.join = ''
         self.payment = ['cash', 'cashapp', 'venmo']
         self.url = 'https://skipthegames.com/posts/'
@@ -16,8 +22,9 @@ class TestScraper:
 
     def initialize(self):
         self.open_webpage()
-        time.sleep(5)
-        self.get_data()
+        time.sleep(3)
+        self.check_post_for_keywords(self.get_data())
+        time.sleep(3)
         self.capture_screenshot()
         self.close_webpage()
 
@@ -29,13 +36,33 @@ class TestScraper:
         self.driver.close()
 
     def get_formatted_url(self) -> str:
-        return f'https://skipthegames.com/posts/{self.location}/?keywords="{self.read_keywords()}"'
+        return f'https://skipthegames.com/posts/{self.location}/"'
 
-    def get_data(self):
-        listings = self.driver.find_element(
-            By.XPATH, '/html/body/div[9]/table')
-        print(listings)
-        print(listings.text)
+    def get_data(self) -> list:
+        self.driver.find_element(
+            By.XPATH, '// *[ @ id = "radio_clsfd_display_mode_single"]').click()
+        time.sleep(2)
+
+        first_post = self.driver.find_element(
+            By.XPATH, '//*[@id="single_view"]/table/tbody/tr[1]/td[1]/a')
+        first_post.click()
+        time.sleep(2)
+
+        table_info = self.driver.find_element(
+            By.XPATH, '/html/body/div[7]/div/div[2]/div/table').text
+        description_div = self.driver.find_element(
+            By.XPATH, '/html/body/div[7]/div/div[2]/div/div[1]/div').text
+        time.sleep(2)
+
+        print([table_info, description_div])
+        print(len([table_info, description_div]))
+        return [table_info, description_div]
+
+    def check_post_for_keywords(self, data):
+        for keyword in self.keywords:
+            if keyword in data[0] or keyword in data[1]:
+                logging.info(data)
+            break
 
     def capture_screenshot(self):
         self.driver.save_screenshot("test_search.png")
