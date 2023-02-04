3a1f87dbd97455807711dd7e622c4dd76367ba01
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2023-02-04 15:53:55 -0500

    search button hardcoded to scrape yesbackpage.

--- GUIComponents/GUIMainWindow.py
+++ GUIComponents/GUIMainWindow.py
@@ -1,19 +1,30 @@
-import sys
-from PyQt6 import QtWidgets, uic
 import qdarkstyle
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from Backend.Facade import Facade
+from MainWindow import Ui_HSIWebScraper
 
+"""Temporarily hardcoded search button to scrape yesbackpacge"""
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        uic.loadUi("MainWindow.ui", self)
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_HSIWebScraper()
+        self.ui.setupUi(self)
         # dark mode
         self.setStyleSheet(qdarkstyle.load_stylesheet('pyqt6'))
 
+        # bind searchButton to search_button_clicked function
+        self.ui.searchButton.clicked.connect(self.search_button_clicked)
+
+    @staticmethod
+    def search_button_clicked():
+        facade = Facade()
+        facade.initialize_yesbackpage_scraper()
+
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec()
