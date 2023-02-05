ca443f1e755777b5f47b267058b6487dfaa77a8d
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2023-02-05 16:44:03 -0500

    added function for website selection functionality

--- GUIComponents/GUIMainWindow.py
+++ GUIComponents/GUIMainWindow.py
@@ -1,26 +1,49 @@
 import qdarkstyle
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from Backend.Facade import Facade
-from MainWindow import Ui_HSIWebScraper
+from MainWindow_ui import Ui_HSIWebScraper
 
 """Temporarily hardcoded search button to scrape yesbackpacge"""
+
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.selection = ''
         self.ui = Ui_HSIWebScraper()
         self.ui.setupUi(self)
+
         # dark mode
         self.setStyleSheet(qdarkstyle.load_stylesheet('pyqt6'))
+
+        # bind websiteSelectionDropdown to website_selection_dropdown function
+        self.ui.websiteSelectionDropdown.currentIndexChanged.connect(self.website_selection_dropdown)
 
         # bind searchButton to search_button_clicked function
         self.ui.searchButton.clicked.connect(self.search_button_clicked)
 
-    @staticmethod
-    def search_button_clicked():
+    def website_selection_dropdown(self):
+        print(self.ui.websiteSelectionDropdown.currentText())
+        self.selection = self.ui.websiteSelectionDropdown.currentText()
+
+    def search_button_clicked(self):
         facade = Facade()
-        facade.initialize_yesbackpage_scraper()
+        if self.selection == 'escortalligator':
+            facade.initialize_escortalligator_scraper()
+        
+        if self.selection == 'megapersonals':
+            facade.initialize_megapersonals_scraper()
+
+        if self.selection == 'skipthegames':
+            facade.initialize_skipthegames_scraper()
+
+        if self.selection == 'yesbackpage':
+            facade.initialize_yesbackpage_scraper()
+
+        # TODO - fix eros bug
+        if self.selection == 'eros':
+            facade.initialize_eros_scraper()
 
 
 if __name__ == "__main__":
