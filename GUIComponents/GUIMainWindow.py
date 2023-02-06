bc9cb2bc053c2019d1a2e002186bea8f7fe09553
Diego Grisales <dfgrisales5078@eagle.fgcu.edu>
2023-02-05 21:10:34 -0500

    search button disabled if no website is selected

--- GUIComponents/GUIMainWindow.py
+++ GUIComponents/GUIMainWindow.py
@@ -1,18 +1,21 @@
+import time
 import qdarkstyle
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from Backend.Facade import Facade
 from MainWindow_ui import Ui_HSIWebScraper
 
-# To make changes to UI: do NOT edit MainWindow_ui.py, make changes to UI using Qt Creator and then run the following
-# command: pyuic6 MainWindow.ui -o MainWindow_ui.py
+# To make changes to UI do NOT edit MainWindow_ui.py, instead make changes to UI using Qt Creator and then run the
+# following command: pyuic6 MainWindow.ui -o MainWindow_ui.py
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.selection = ''
         self.ui = Ui_HSIWebScraper()
         self.ui.setupUi(self)
+
+        # stores website selection:
+        self.selection = ''
 
         # dark mode
         self.setStyleSheet(qdarkstyle.load_stylesheet('pyqt6'))
@@ -20,26 +23,32 @@ class MainWindow(QMainWindow):
         # bind websiteSelectionDropdown to website_selection_dropdown function
         self.ui.websiteSelectionDropdown.currentIndexChanged.connect(self.website_selection_dropdown)
 
+        # disable button until website is selected
+        self.ui.searchButton.setEnabled(False)
         # bind searchButton to search_button_clicked function
         self.ui.searchButton.clicked.connect(self.search_button_clicked)
 
     def website_selection_dropdown(self):
-        print(self.ui.websiteSelectionDropdown.currentText())
         self.selection = self.ui.websiteSelectionDropdown.currentText()
+        self.ui.searchButton.setEnabled(True)
 
     def search_button_clicked(self):
         facade = Facade()
         if self.selection == 'escortalligator':
             facade.initialize_escortalligator_scraper()
-        
+            time.sleep(2)
+
         if self.selection == 'megapersonals':
             facade.initialize_megapersonals_scraper()
+            time.sleep(2)
 
         if self.selection == 'skipthegames':
             facade.initialize_skipthegames_scraper()
+            time.sleep(2)
 
         if self.selection == 'yesbackpage':
             facade.initialize_yesbackpage_scraper()
+            time.sleep(2)
 
         # TODO - fix eros bug
         if self.selection == 'eros':
