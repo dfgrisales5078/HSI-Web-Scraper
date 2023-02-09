ac78d2189e7ff68f88cd47d5541a83dd304a877e
Diego Grisales <54412831+dfgrisales5078@users.noreply.github.com>
2023-02-09 09:59:47 -0500

    Updated features to GUI (#20)
    * added function to add and remove keywords from QlistWidget
    
    * added function to add and remove keywords from QlistWidget
    
    * added function to initialize list to keywords from keywords.txt
    
    * added function to initialize list to keywords from keywords.txt
    
    * add and remove keywords from gui and text file
    
    * edited variable name for consistency
    
    * added secondary tab to enable adding and removing keywords from GUI
    
    * changes made up update GUI features
    
    * Added TODO comments and improved textbox functionalities
    
    * commented out main on Keywords.py

--- Backend/Keywords.py
+++ Backend/Keywords.py
@@ -8,52 +8,59 @@ class Keywords:
 
     @staticmethod
     def add_keywords(keyword):
-        with open("keywords.txt", "r+") as filename:
-            contentSet = filename.read().splitlines()
-            if keyword not in contentSet:
+        with open("../keywords.txt", "r+") as filename:
+            content_set = filename.read().splitlines()
+            if keyword not in content_set:
                 filename.write("\n" + keyword.lower())
             else:
                 print("Keyword already in file")
 
     @staticmethod
     def remove_keywords(keyword):
-        with open("keywords.txt", "r") as filename:
-            contentSet = filename.read().splitlines()
-            index = contentSet.index(keyword.lower())
+        with open("../keywords.txt", "r") as filename:
+            content_set = filename.read().splitlines()
+            index = content_set.index(keyword.lower())
 
-            contentSet.remove(contentSet[index])
+            content_set.remove(content_set[index])
 
-        with open("keywords.txt", "w") as filename:
-            filename.write("\n".join(contentSet))
+        with open("../keywords.txt", "w") as filename:
+            filename.write("\n".join(content_set))
 
-    def create_set(self, setName, keywordsList):
-        with open("keyword_sets.txt", "r") as readfile:
+    def create_set(self, set_name, keywords_list):
+        with open("../keyword_sets.txt", "r") as readfile:
             read = readfile.read()
             if read != '':
                 self.sets = json.loads(read)
-        self.sets[setName.lower()] = keywordsList.split(', ')
-        with open("keyword_sets.txt", "w") as writefile:
+        self.sets[set_name.lower()] = keywords_list
+        with open("../keyword_sets.txt", "w") as writefile:
             json.dump(self.sets, writefile)
 
-    def remove_set(self, setName):
-        with open("keyword_sets.txt", "r") as readfile:
+    def remove_set(self, set_name):
+        with open("../keyword_sets.txt", "r") as readfile:
             self.sets = json.loads(readfile.read())
 
-        del self.sets[setName.lower()]
+        del self.sets[set_name.lower()]
 
-        with open('keyword_sets.txt', 'w') as writefile:
+        with open('../keyword_sets.txt', 'w') as writefile:
             json.dump(self.sets, writefile)
 
+    def get_set_values(self, set_name):
+        with open("../keyword_sets.txt", "r") as readfile:
+            self.sets = json.loads(readfile.read())
+
+        return self.sets[set_name.lower()]
+
     def get_keywords(self):
-        with open("keywords.txt", "r") as filename:
+        with open("../keywords.txt", "r") as filename:
             self.keywords = filename.read().splitlines()
         return self.keywords
 
     def get_set(self):
-        with open("keyword_sets.txt", "r") as filename:
+        with open("../keyword_sets.txt", "r") as filename:
             self.sets = json.loads(filename.read())
 
         return self.sets
+
 
 # code to test if the keywords class is working
 # if __name__ == '__main__':
@@ -66,6 +73,7 @@ class Keywords:
 #                           "Type 4 to remove a set\n"
 #                           "Type 5 to view keywords\n"
 #                           "Type 6 to view sets\n"
+#                           "Type 7 to view values of a set\n"
 #                           "Type 0 to quit\n"
 #                           "Enter input: ")
 #         if userInput == '1':
@@ -87,5 +95,8 @@ class Keywords:
 #             print(key.get_keywords())
 #         elif userInput == '6':
 #             print(key.get_set())
+#         elif userInput == '7':
+#             st = input("enter set to get values: ")
+#             print(key.get_set_values(st))
 #         else:
 #             break
--- GUIComponents/GUIMainWindow.py
+++ GUIComponents/GUIMainWindow.py
@@ -1,11 +1,11 @@
 import time
-from PyQt6 import QtWidgets
 from Backend.Facade import Facade
-from MainWindow_ui import Ui_HSIWebScraper
+from ui_Scraper import Ui_HSIWebScraper
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from Backend.Keywords import Keywords
 
-# To make changes to UI do NOT edit MainWindow_ui.py, instead make changes to UI using Qt Creator and then run the
-# following command: pyuic6 MainWindow.ui -o MainWindow_ui.py
+
+# To make changes to UI do NOT edit MainWindow_ui.py, instead make changes to UI using Qt Creator.
 
 class MainWindow(QMainWindow):
 
@@ -13,6 +13,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_HSIWebScraper()
         self.ui.setupUi(self)
+        self.keywords_instance = Keywords()
 
         # TODO - center app when screen maximized
         # self.central_widget = QWidget(self)
@@ -25,11 +26,19 @@ class MainWindow(QMainWindow):
         self.website_selection = ''
         self.include_payment_method = ''
         self.search_text = ''
+        self.keywords = self.keywords_instance.get_keywords()
+        self.keyword_sets = self.keywords_instance.get_set()
         self.keywords_selected = set()
+        self.keys_to_add_to_new_set = []
+        self.location = ''
+
+        self.initialize_keywords(self.keywords)
+        self.initialize_keyword_sets(self.keyword_sets)
 
         # dark mode
         # self.setStyleSheet(qdarkstyle.load_stylesheet('pyqt6'))
 
+        ''' Bind GUI components to functions: '''
         # bind websiteSelectionDropdown to website_selection_dropdown function
         self.ui.websiteSelectionDropdown.currentIndexChanged.connect(self.website_selection_dropdown)
 
@@ -50,29 +59,161 @@ class MainWindow(QMainWindow):
         # bind keywordlistWidget to keyword_list_widget function
         self.ui.keywordlistWidget.itemClicked.connect(self.keyword_list_widget)
 
-        # bind toolButton to tool_button_clicked function
-        self.ui.toolButton.clicked.connect(self.tool_button_clicked)
-
         # bind keywordInclusivecheckBox to keyword_inclusive_check_box function
         self.ui.keywordInclusivecheckBox.stateChanged.connect(self.keyword_inclusive_check_box)
 
         # bind setSelectionDropdown to set_selection_dropdown function
         self.ui.setSelectionDropdown.currentIndexChanged.connect(self.set_selection_dropdown)
 
+        # bind addKeywordButton to add_keyword_button_clicked function
+        self.ui.addKeywordButton.clicked.connect(self.add_keyword_button_clicked)
+
+        # bind removeKeywordButton to remove_keyword_button_clicked function
+        self.ui.removeKeywordButton.clicked.connect(self.remove_keyword_button_clicked)
+
+        # bind addSetButton to add_set_button_clicked function
+        self.ui.addSetButton.clicked.connect(self.add_set_button_clicked)
+        # TODO - disable addSetButton until new set name is entered?
+
+        # bind removeSetButton to remove_set_button_clicked function
+        self.ui.removeSetButton.clicked.connect(self.remove_set_button_clicked)
+
+        # bind locationTextBox to location_text_box function
+        self.ui.setlocationDropdown.currentIndexChanged.connect(self.location_text_box)
+
     ''' Functions used to handle events: '''
 
-    # TODO - find way to edit list, remove keyword if it's manually unselected
+    # remove set when button is clicked
+    def remove_set_button_clicked(self):
+        # get name of set to remove
+        set_to_remove = self.ui.setList.currentItem().text()
+
+        # remove set from keyword_sets
+        self.keywords_instance.remove_set(set_to_remove)
+
+        # remove set from GUI
+        self.ui.setList.takeItem(self.ui.setList.currentRow())
+
+        # update list of sets
+        self.ui.setSelectionDropdown.clear()
+        self.ui.setSelectionDropdown.addItems(self.keyword_sets)
+
+    # TODO - get locations based on scraper selected 
+    def location_text_box(self):
+        self.location = self.ui.setlocationDropdown.currentText()
+
+    # add new set when button is clicked
+    def add_set_button_clicked(self):
+        # get name of new set
+        new_set_name = self.ui.newSetTextBox.text()
+
+        # get text from list
+        for item in self.ui.keywordList.selectedItems():
+            self.keys_to_add_to_new_set.append(item.text())
+
+        # call create_set function from keywords class
+        self.keywords_instance.create_set(new_set_name, self.keys_to_add_to_new_set)
+
+        # update list of sets
+        self.ui.setList.addItem(new_set_name)
+
+        # make self.keys_to_add_to_new_set of an empty list
+        self.keys_to_add_to_new_set = []
+
+        # add new set to setSelectionDropdown
+        self.ui.setSelectionDropdown.addItem(new_set_name)
+
+        # clear new set text box
+        self.ui.newSetTextBox.clear()
+
+    # TODO - loop through sets and remove keyword from each set 
+    # BUG - if multiple keywords are selected and one is removed, the other keywords are not removed from the set
+    # remove new keyword
+    def remove_keyword_button_clicked(self):
+        # find text of selected item
+        keyword = self.ui.keywordList.currentItem().text()
+
+        if keyword != '':
+            self.remove_keyword(keyword)
+
+    # add new keyword
+    def add_keyword_button_clicked(self):
+        keyword = self.ui.newKeywordTextBox.text()
+
+        if keyword != '':
+            self.ui.keywordlistWidget.addItem(keyword)
+            self.ui.keywordList.addItem(keyword)
+            # add keyword to text file
+            self.keywords_instance.add_keywords(keyword)
+
+            # clear new keyword text box
+            self.ui.newKeywordTextBox.clear()
+
+    # initialize all keywords to scraper tab
+    def initialize_keywords(self, keywords):
+        for keyword in keywords:
+            self.ui.keywordlistWidget.addItem(keyword)
+            self.ui.keywordList.addItem(keyword)
+
+    # initialize all keyword sets to GUI
+    def initialize_keyword_sets(self, keyword_sets):
+        for keyword_set in keyword_sets:
+            self.ui.setSelectionDropdown.addItem(keyword_set)
+            self.ui.setList.addItem(keyword_set)
+
+    # remove keyword from keywordlistWidget
+    def remove_keyword(self, keyword):
+        # remove keyword from keywordlistWidget using keyword text
+        for i in range(self.ui.keywordlistWidget.count()):
+            if self.ui.keywordlistWidget.item(i).text() == keyword:
+                self.ui.keywordlistWidget.takeItem(i)
+                self.ui.keywordList.takeItem(i)
+
+                # remove from text file
+                self.keywords_instance.remove_keywords(keyword)
+                break
+
+    # handle list of keywords to be searched
     def keyword_list_widget(self):
         # print('selected keywords:')
         for item in self.ui.keywordlistWidget.selectedItems():
             print(item.text())
             self.keywords_selected.add(item.text())
+
+        # if a keyword is unselected, remove it from the set
+        for i in range(self.ui.keywordlistWidget.count()):
+            if not self.ui.keywordlistWidget.item(i).isSelected():
+                self.keywords_selected.discard(self.ui.keywordlistWidget.item(i).text())
+
         print(self.keywords_selected)
 
-
-    # TODO - find way to edit list of sets
+    # handle dropdown menu for keyword sets
     def set_selection_dropdown(self):
-        print(self.ui.setSelectionDropdown.currentText())
+        selected_set = self.ui.setSelectionDropdown.currentText()
+        print(selected_set)
+
+        # if empty set, unselect all keywords
+        if selected_set == '':
+            for i in range(self.ui.keywordlistWidget.count()):
+                self.ui.keywordlistWidget.item(i).setSelected(False)
+            return
+
+        # get keywords from selected set from keywords class
+        keyword_in_set = self.keywords_instance.get_set_values(selected_set)
+
+        print(keyword_in_set)
+
+        # select keyword_in_set in keywordlistWidget
+        for i in range(self.ui.keywordlistWidget.count()):
+            if self.ui.keywordlistWidget.item(i).text() in keyword_in_set:
+                self.ui.keywordlistWidget.item(i).setSelected(True)
+            else:
+                self.ui.keywordlistWidget.item(i).setSelected(False)
+
+        # unselect all keywords in keywordlistWidget keyword_in_set is empty
+        if not keyword_in_set:
+            for i in range(self.ui.keywordlistWidget.count()):
+                self.ui.keywordlistWidget.item(i).setSelected(False)
 
     # TODO - add logic to scrape with search text
     def search_text_box(self):
@@ -91,6 +232,7 @@ class MainWindow(QMainWindow):
         # enable checkbox after it's unchecked
         self.ui.keywordInclusivecheckBox.setEnabled(True)
 
+    # if checked, select all items in list widget
     def select_all_keywords_check_box(self):
         if self.ui.selectAllKeywordscheckBox.isChecked():
             self.ui.selectAllKeywordscheckBox.setEnabled(True)
@@ -126,20 +268,13 @@ class MainWindow(QMainWindow):
         # enable checkbox after it's unchecked
         self.ui.paymentMethodcheckBox.setEnabled(True)
 
+    # handle dropdown menu for payment method
     def website_selection_dropdown(self):
         self.website_selection = self.ui.websiteSelectionDropdown.currentText()
         print(self.ui.websiteSelectionDropdown.currentText())
         self.ui.searchButton.setEnabled(True)
 
-    # TODO - edit popup window in QT Creator
-    def tool_button_clicked(self):
-        print('tool button clicked')
-        # pop up window
-        self.popup = QtWidgets.QWidget()
-        self.popup.setGeometry(100, 100, 400, 200)
-        self.popup.setWindowTitle('Edit Keywords and sets')
-        self.popup.show()
-
+    # scrape website selected when search button is clicked
     def search_button_clicked(self):
         facade = Facade()
         if self.website_selection == 'escortalligator':
